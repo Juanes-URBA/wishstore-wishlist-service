897da231548d6a7d9ed8da4eb16b936f10f00e49
Juanes_URBA <024juanurrego@gmail.com>
2026-08-05 10:58:48 -0500

    chore: initialize repository

---
+++ src/main/java/com/wishstore/wishlist/WishstoreWishlistServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wishstore.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishstoreWishlistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishstoreWishlistServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wishstore/wishlist/WishstoreWishlistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishstore.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishstoreWishlistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
